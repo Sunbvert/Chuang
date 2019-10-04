d71bedf9e0b52b30abed9863a62d5d2319bfd1cc
sunbvert <sunchuanop@outlook.com>
2019-10-04 17:56:12 +0800

    learned gl indices square

--- SimulationWorld/main.cpp
+++ SimulationWorld/main.cpp
@@ -11,13 +11,96 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-int main(int argc, const char * argv[]) {
+#include <fstream>
+#include <string>
+#include <sstream>
+
+struct ShaderProgramSource {
+    std::string VertexSource;
+    std::string FragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const std::string& filepath) {
+    std::cout << filepath << std::endl;
+    system("pwd");
+    std::ifstream stream(filepath);
+    
+    if (!stream)
+        std::cout << "Faile to read shader file!" << std::endl;
+    
+    enum class ShaderType {
+        NONE = -1, VERTEX = 0, FRAGMENT = 1
+    };
+    
+    std::string line;
+    std::stringstream ss[2];
+    ShaderType type = ShaderType::NONE;
+    while (getline(stream, line)) {
+        if (line.find("#shader") != std::string::npos) {
+            if (line.find("vertex") != std::string::npos)
+                type = ShaderType::VERTEX;
+            else if (line.find("fragment") != std::string::npos)
+                type = ShaderType::FRAGMENT;
+        } else {
+            ss[(int)type] << line << '\n';
+        }
+    }
+    return { ss[0].str(), ss[1].str() };
+}
+
+static unsigned int CompileShader(unsigned int type, const std::string& source) {
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+    
+    // Error Handling
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE) {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment")
+        << " shader!" << std::endl;
+        std::cout << message << std::endl;
+        
+        glDeleteShader(id);
+        return 0;
+    }
+    
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader){
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+    
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+    
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+    
+    return program;
+}
+
+int main(void) {
     GLFWwindow *window;
     
     // initialize the library
     if ( !glfwInit() ) {
         return -1;
     }
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     
     // create a windowed mode window and its OpenGL context
     window = glfwCreateWindow(640, 480, "Simulator", NULL, NULL);
@@ -29,12 +112,66 @@ int main(int argc, const char * argv[]) {
     
     // make the window's context current
     glfwMakeContextCurrent( window );
+    glfwSwapInterval(1);
+    
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+        printf("Error!\n");
+    
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+    
+    std::cout << glGetString(GL_VERSION) << std::endl;
+    
+    
+    float positions[] = {
+        -0.5f, -0.5f,
+         0.5f, -0.5f,
+         0.5f,  0.5f,
+        -0.5f,  0.5f
+    };
+    
+    unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+    
+    unsigned int vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 4 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+    
+    ShaderProgramSource source = ParseShader("../Res/Shader/Basic.shader");
+    
+    unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
+    glUseProgram(shader);
+    
+    for(GLenum err; (err = glGetError()) != GL_NO_ERROR;)
+    {
+        std::cerr << "OpenGL error: " << err << std::endl;
+    }
     
     // loop until the user closes the widow
     while( !glfwWindowShouldClose( window ) ) {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
         // render the OpenGL, put all the OpenGL code right here
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        
         
         // swap front and back buffers
         glfwSwapBuffers(window);
@@ -42,6 +179,8 @@ int main(int argc, const char * argv[]) {
         // pool for and process events
         glfwPollEvents();
     }
+    
+    glDeleteProgram(shader);
     
     glfwDestroyWindow(window);
     glfwTerminate();
